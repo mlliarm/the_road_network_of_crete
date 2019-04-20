0287ced1708d9e8677fe947c591a36897664707c
milia <mlliarm@yandex.com>
2019-04-20 18:34:55 +0300

    created drawing function for the network

--- model_cretan_road_network.py
+++ model_cretan_road_network.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import sys
 
 def parse_edges(input_filename):
     with open(input_filename, 'r') as handle:
@@ -18,45 +19,68 @@ def create_network(list_of_edges):
     G.add_edges_from(list_of_edges)
     return G
 
+def draw_network(G, type_of_network=None):
+    if type_of_network == "planar":
+        nx.draw_planar(G, with_labels = True)
+    elif type_of_network == "circular":
+        nx.draw_circular(G, with_labels = True)
+    elif type_of_network == "random":
+        nx.draw_random(G, with_labels = True)
+    elif type_of_network == "spectral":
+        nx.draw_random(G, with_labels = True)
+    elif type_of_network == "kamada_kawai":
+        nx.draw_kamada_kawai(G, with_labels = True)
+    elif type_of_network == "spring":
+        nx.draw_spring(G, with_labels = True)
+    elif type_of_network == "shell":
+        nx.draw_shell(G, with_labels = True)
+    else:
+        nx.draw(G, with_labels = True)
+    plt.savefig("network_" + str(type_of_network) + ".jpg")
 
 def main():
-    list_of_edges = parse_edges("crt_edges_wo_weights.txt")
-    print(list_of_edges)
+    try:
+        type_of_network = sys.argv[1]
+    except IndexError:
+        print("Please insert type of network from the below:")
+        print("planar")
+        print("circular")
+        print("random")
+        print("spectral")
+        print("kamada_kawai")
+        print("spring")
+        print("shell")
+        print("Type: python3 model_cretan_road_network.py type_of_network")
+        return [0, "IndexError"]
 
+    list_of_edges = parse_edges("crt_edges_wo_weights.txt")
+
+    # Creation of the network
     G = create_network(list_of_edges)
 
-    options = options = {
-            'node_color': 'red',
-            'node_size': 10,
-            'width': 1,
-            }
-    #plt.subplot(221)
-    #fig1 = plt.figure()
-    #nx.draw_random(G, with_labels=True, **options)
-    #fig1.savefig('crete_random.jpg')
-    #plt.subplot(222)
-    #fig2 = plt.figure()
-    #nx.draw_circular(G, with_labels=True, **options)
-    #fig2.savefig('crete_circular.jpg')
-    #plt.subplot(223)
-    #fig3 = plt.figure()
-    #nx.draw_spectral(G,with_labels=True, **options)
-    #fig3.savefig('crete_spectral.jpg')
-    #plt.subplot(224)
-    #nx.draw_shell(G, nlist=[range(7,10), range(68)], **options)
-    #plt.show()
-
+    # Calculation of centrality measures
     DC = nx.algorithms.degree_centrality(G)
-    print(DC)
     CC = nx.algorithms.closeness_centrality(G)
-    print(CC)
     BC = nx.algorithms.betweenness_centrality(G)
-    print(BC)
     EC = nx.algorithms.eigenvector_centrality(G)
-    print(EC)
     HC = nx.algorithms.harmonic_centrality(G)
-    print(HC)
     PC = nx.algorithms.pagerank(G)
-    print(PC)
-main()
+
+    # Clustering
+    clustering = nx.algorithms.clustering(G)
+
+    # Printing info of graph
+    print(nx.info(G))
+
+    draw_network(G, type_of_network)
+
+    N, K = G.order(), G.size()
+    avg_deg = float(K)/N
+
+    print("Nodes:",N)
+    print("Edges:",K)
+    print("Average degree:",avg_deg)
+
+if __name__ == "__main__":
+    main()
 
